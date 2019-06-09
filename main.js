62dc2ecd30f800a88e83b88509204a82bf3e5144
FernandoSLuz <fernandosilvadaluz@gmail.com>
2019-06-09 08:06:05 -0300

    check validation

--- main.js
+++ main.js
@@ -63,7 +63,7 @@ class Blockchain{
 let jsChain = new Blockchain();
 jsChain.addBlock(new Block("12/25/2017", {amount: 5}));
 jsChain.addBlock(new Block("12/26/2017", {amount: 10}));
-jsChain.valid = true;
+jsChain.valid = jsChain.checkValid();
 
 
 
